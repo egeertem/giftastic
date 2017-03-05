46063ced8b29b79da4f64e10e580f861c89372ea
Ege <ertem@ephesusventures.com>
2017-03-05 04:26:11 -0800

    Real Starter

---
+++ assets/javascript/app.js
@@ -0,0 +1,98 @@
+
+/*
+Problem: We would like to have top 10 Gifs from giffy site to be presented according to a handful of (ok, maybe 15) pre-defined categories, up on click to a given category, top ten gifs how up. We would like to be able to add extra categories as required. 
+
+Target users: Zach and Ege and my wife. 
+
+Interface: Buttons that will be on top of the page.
+
+
+Testing: Simple funtionality testing no corner cases. 
+
+Maintainer:
+
+Ege Ertem, email to ertem@ephesusventures.com 
+
+Functional requirements: 
+
+	// The player will have an access to 20 type of gif categories. 
+    // When clicked any of those categories top 10 most popular gifs are being displayed. 
+	// There is an area where you can enter extra new category.... 
+
+
+	// Sequence chart === > State Diagram === > Flow chart ===> Pseudo code.   
+
+	//   					Variables come with the State diagram.
+
+//******** State starts *************
+
+/
+//Varaiables: 
+
+
+
+// State 1 / (Initial) 
+    // 20 catergories are listed on top as buttons... 
+    //10 top gifs are listed from Erdogan
+
+// Transition:
+
+
+//State 2 / as a cont of state 1..... 
+
+
+
+// You can  just click on any category you want and be able to get what ever you want. 
+
+// Transition:
+
+//State 3 / Add any new category from the in put provided to you. 
+
+
+
+Trivia states  | behaviour
+---------------------------------------------------
+State 1 / Initial            | Display Start and timer @ 30 sec.
+                             | rigthAnswers: 0;
+                             | wrongAnswers: 0; 
+                             | funtion() countdown(): not started
+                             | 
+                             |  
+
+
+
+
+Trivia states  | behaviour
+---------------------------------------------------
+State 2 / Initial            | Display Start and timer @ 30 sec.
+                             | rigthAnswers: 0;
+                             | wrong answers: 0; 
+                             | funtion() countdown(): going down. 
+
+Trivia states  | behaviour
+---------------------------------------------------
+State 3 / Initial            | Display: 
+                             | # of right answers: rigthAnswers;
+                             | # of wrong answers: wrong answers; 
+                             | funtion() countdown(): at zero.
+
+
+
+    
+    
+
+
+
+//******** State starts *************
+
+// State 1 / (Initial) 
+    // 10 catergories are listed on top as buttons..
+
+    //Get the Json for Pandas via Gifty API 
+
+    //Put them to a div... 
+    
+*/
+
+
+
